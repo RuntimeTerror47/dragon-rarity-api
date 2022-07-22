c5670824200eb368ca4deefcece752536efbd3f5
RuntimeTerror47 <emrekeskin47@icloud.com>
2022-07-22 13:37:58 +0300

    random temp

--- routes/dragons.js
+++ routes/dragons.js
@@ -3,16 +3,25 @@ var router = express.Router();
 var { CosmWasmClient } = require("@cosmjs/cosmwasm-stargate");
 
 const CONTRACT_ADDRESS =
-    "juno1segt88gmnpj4fc30h8jczetjw5zz58qlqgk6tty2kamdg7kn3x0sfv3j9x";
+    "juno100dkyna0r52pawt229xdsfzr29rxr9wf2f4xgapzn72aqcm905rqc93xgw";
 const RPC_URL = "https://rpc.juno.giansalex.dev";
+
+const getDragonType = (id) => {
+    let type = Number(id) % 5;
+    if (type === 0) return "Legendary";
+    if (type === 2) return "Uncommon";
+    if (type === 3) return "Rare";
+    if (type === 4) return "Epic";
+    else return "Common";
+};
 
 /* GET users listing. */
 router.get("/:address", async function (req, res, next) {
     let owner = req.params["address"];
     let tokenRes;
     let error = false;
+    const client = await CosmWasmClient.connect(RPC_URL);
     try {
-        const client = await CosmWasmClient.connect(RPC_URL);
         tokenRes = await client.queryContractSmart(CONTRACT_ADDRESS, {
             Tokens: { owner: owner },
         });
@@ -26,10 +35,14 @@ router.get("/:address", async function (req, res, next) {
     } else {
         let response = [];
         for (let i = 0; i < tokenRes.tokens.length; i++) {
-            response.push({
+            // let type = await client.queryContractSmart(CONTRACT_ADDRESS, {
+            //     DragonInfo: { id: tokenRes.tokens[i] },
+            // });
+            // console.log(type);
+            dragonType = response.push({
                 id: tokenRes.tokens[i],
                 imageUrl: "...",
-                rarity: "Common",
+                rarity: getDragonType(tokenRes.tokens[i]),
             });
         }
         res.send(JSON.stringify({ dragons: response }));
